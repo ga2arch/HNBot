ac91b028ef5f6cc65d0f87b7488b42eb81104002
Gabriele Carrettoni <gabriele.carrettoni@gmail.com>
2015-06-30 16:38:21 +0200

    added getUsers to fetch users from the redis db

--- src/Bot.hs
+++ src/Bot.hs
@@ -7,10 +7,39 @@ import Control.Concurrent.MVar
 import Control.Monad.Catch
 import Control.Monad.Reader
 import Control.Monad.IO.Class (MonadIO, liftIO)
-import Data.Text (Text)
+import Data.Aeson
+import Data.Maybe (fromJust)
+import GHC.Generics
 import Network.HTTP.Base (urlEncodeVars)
 import Network.HTTP.Client
 import Network.HTTP.Client.TLS
+
+import qualified Database.Redis as R
+import qualified Data.ByteString.Char8 as C
+
+data User = User {
+    id :: Int
+} deriving (Show, Generic)
+
+instance FromJSON User
+instance ToJSON   User
+
+data Message = Message {
+    message_id :: Int
+,   from :: User
+,   text :: Maybe String
+} deriving (Show, Generic)
+
+instance FromJSON Message
+instance ToJSON   Message
+
+data Update = Update {
+    update_id :: Int
+,   message :: Maybe Message
+} deriving (Show, Generic)
+
+instance FromJSON Update
+instance ToJSON   Update
 
 newtype Bot a = B { unBot :: ReaderT BotData IO a }
     deriving (Functor, Applicative, Monad, MonadReader BotData,
@@ -20,11 +49,12 @@ data BotData = BotData BotConfig BotState
 
 data BotConfig = BotConfig {
     botPort    :: Int
-,   botToken   :: Text
+,   botToken   :: String
 ,   botManager :: Manager
+,   botDbConn  :: R.Connection
 }
 
-data BotState = forall a. (Show a) => BotState {
+data BotState = forall a. BotState {
     botCache :: MVar a
 }
 
@@ -34,11 +64,32 @@ runBot bdata f = runReaderT (unBot f) bdata
 getPort :: Bot Int
 getPort = asks $ \(BotData c _) -> botPort c
 
-getToken :: Bot Text
+getToken :: Bot String
 getToken = asks $ \(BotData c _) -> botToken c
 
 getManager :: Bot Manager
 getManager = asks $ \(BotData c _) -> botManager c
+
+getDbConn :: Bot R.Connection
+getDbConn = asks $ \(BotData c _) -> botDbConn c
+
+getUsers :: Bot [(User, C.ByteString)]
+getUsers = do
+    conn <- getDbConn
+    users <- liftIO $ R.runRedis conn $ do
+        ks <- query
+        mapM (\k -> do
+            Right t <- R.get k
+            return (User . toInt $ k,
+                    fromJust t)) ks
+    return users
+  where
+    query = do
+        Right ks <- R.keys "*"
+        return ks
+
+    toInt :: C.ByteString -> Int
+    toInt = read . C.unpack
 
 withCache :: (forall a. a -> Bot (a, b)) -> Bot b
 withCache f = do
@@ -46,13 +97,30 @@ withCache f = do
     withCache' d f
   where
       withCache' :: BotData -> (forall a. a -> Bot (a, b)) -> Bot b
-      withCache' (BotData _ (BotState m)) f = do
+      withCache' (BotData _ (BotState m)) f =
           mask $ \restore -> do
               cache <- liftIO $ takeMVar m
               (newCache, result) <- restore (f cache)
                 `onException` (liftIO $ putMVar m cache)
               liftIO $ putMVar m newCache
               return result
+
+send :: String -> User -> Bot ()
+send text u@(User uid) = do
+    m     <- getManager
+    token <- getToken
+
+    let baseUrl = "https://api.telegram.org/bot"
+    let payload = urlEncodeVars [("chat_id", (show uid)),
+                                ("text", text),
+                                ("disable_web_page_preview", "true")]
+    let url = mconcat [baseUrl, token, "/sendMessage?", payload]
+
+    req <- parseUrl url
+    r <- liftIO $ try $ httpLbs req m
+    case r of
+        Left (ex :: SomeException) -> liftIO $ print ex
+        Right _  -> return ()
 
 test :: Bot ()
 test = do
