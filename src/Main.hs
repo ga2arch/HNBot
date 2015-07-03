4f28d8dc6c3b413673c3dcbe46747248182de635
Gabriele Carrettoni <gabriele.carrettoni@gmail.com>
2015-07-03 20:14:18 +0200

    added hackernews stuff

--- src/Main.hs
+++ src/Main.hs
@@ -1,10 +1,11 @@
-{-# LANGUAGE OverloadedStrings, DeriveGeneric, ScopedTypeVariables,
-             FlexibleContexts, RankNTypes, ExistentialQuantification #-}
+{-# LANGUAGE OverloadedStrings, DeriveGeneric, RecordWildCards,
+             FlexibleContexts #-}
 module Main where
 
 import Control.Concurrent
 import Control.Monad
 import Control.Concurrent.MVar
+import Control.Exception
 import Control.Monad.IO.Class
 import Control.Monad.Trans.Class
 import Data.List
@@ -20,10 +21,12 @@ import HackerNews
 import qualified Data.Map as M
 import qualified Text.Parsec as P
 import qualified Data.ByteString.Char8 as C
+import qualified Database.Redis as R
 
 data Cache = Cache {
     cacheStories :: M.Map Int Story
 ,   cacheIds     :: [Int]
+,   cacheAlreadySent :: [Int]
 }
 
 helpCmd = do
@@ -39,44 +42,66 @@ topN n cache = do
     ids <- liftIO $ getTopN m n
     sendStories cache ids
 
+threshold = do
+    P.string $ "/threshold"
+
+    send "Threshold: "
+
+    next $ do
+        t <- read <$> P.many1 P.digit
+        when (t <= 20) $ do
+            conn <- lift $ getDbConn
+            User uid <- P.getState
+
+            liftIO $ R.runRedis conn $ do
+                Right u <- R.get $ C.pack . show $ uid
+                case u of
+                    Just _ ->
+                        R.set (C.pack $ show uid) (C.pack $ show t)
+                        >> return ()
+                    Nothing -> return ()
+
 ancor cache = do
     m <- getManager
     users <- getUsers
 
     forever $ do
-        Cache _ oldTop <- liftIO $ readMVar cache
+        Cache {..} <- liftIO $ readMVar cache
         temp <- liftIO $ getTopStories m
 
         case temp of
             Just newTop ->
                 forM_ users $ \(user, tbyte) -> do
                     let threshold = read $ C.unpack tbyte
-                    let tOldTop = take threshold oldTop
+                    let tOldTop = take threshold cacheIds
                     let tNewTop = take threshold newTop
 
-                    let diff = tNewTop \\ tOldTop
+                    let diff = (tNewTop \\ tOldTop) \\ cacheAlreadySent
                     mapM_ (\i -> do
                         s <- getStory' m cache i
                         (flip send') user . title . fromJust $ s) diff
 
                     liftIO $ modifyMVar_ cache $ \c ->
-                        return $ c { cacheIds = newTop }
+                        return $ c {
+                            cacheIds = newTop
+                        ,   cacheAlreadySent = cacheAlreadySent ++ diff
+                        }
 
             Nothing -> return ()
         liftIO $ threadDelay $ 60 * 1000 * 1000
         return ()
 
 getStory' m cache sid =
-    liftIO $ modifyMVar cache $ \c@(Cache stories ids) -> do
-        if sid `M.member` stories
-            then return (Cache stories ids, Just $ stories M.! sid)
+    liftIO $ modifyMVar cache $ \c@(Cache {..}) -> do
+        if sid `M.member` cacheStories
+            then return (c, Just $ cacheStories M.! sid)
             else do
                 story <- getStory m sid
                 case story of
                     Just s -> do
-                        let nStories = M.insert sid s stories
-                        return (Cache nStories ids, story)
-                    Nothing -> return (Cache stories ids, Nothing)
+                        let nStories = M.insert sid s cacheStories
+                        return (c { cacheStories = nStories }, story)
+                    Nothing -> return (c, Nothing)
 
 sendStories cache ids = do
     m <- lift $ getManager
@@ -94,13 +119,14 @@ main = do
         let config = BotConfig 8080 token manager conn
 
         ids <- fmap fromJust $ getTopStories manager
-        cache <- newMVar $ Cache M.empty ids
+        cache <- newMVar $ Cache M.empty ids []
 
         runBot config $ do
             addCmd helpCmd
             addCmd $ topN 3 cache
             addCmd $ topN 5 cache
             addCmd $ topN 10 cache
+            addCmd $ threshold
 
             server
             handler "first"
