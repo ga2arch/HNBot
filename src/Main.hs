118d7164e4d8aeb91cd036d067708d5ba44997d0
Gabriele Carrettoni <gabriele.carrettoni@gmail.com>
2015-07-06 15:50:20 +0200

    moved commands inside their files

--- src/Main.hs
+++ src/Main.hs
@@ -2,32 +2,17 @@
              FlexibleContexts, ScopedTypeVariables #-}
 module Main where
 
-import Control.Concurrent
-import Control.Concurrent.Async
-import Control.Concurrent.MVar
-import Control.Exception
-import Control.Monad
-import Control.Monad.IO.Class
-import Control.Monad.Trans.Class
-import Data.List
-import Data.String.Utils (replace, endswith)
+import Control.Concurrent.MVar (newMVar)
 import Data.Maybe
 import Database.Redis (connect, defaultConnectInfo)
-import Network.HTTP.Base (urlEncode)
 import Network.HTTP.Client
 import Network.HTTP.Client.TLS
-import System.Process
-import System.Directory
-import System.Random (randomRIO)
 
 import Web.Bot
 import Web.Bot.Commands
 import Web.HackerNews
 
 import qualified Data.Map as M
-import qualified Text.Parsec as P
-import qualified Data.ByteString.Char8 as C
-import qualified Database.Redis as R
 
 main = do
     conn <- connect defaultConnectInfo
